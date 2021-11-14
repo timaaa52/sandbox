b05799e26f0937b8950ff0108a7e6038d321e166
Tima <tima52rus@gmail.com>
2021-11-14 13:13:15 +0200

    ss

---
+++ cod_wars/Basic_Mathematical_Operations/index.js
@@ -0,0 +1,4 @@
+function basicOp(oper, a, b) {
+    // Code
+    if (oper == '+') { return a + b } else if (oper == '-') { return a - b } else if (oper == '*') { return a * b } else if (oper == '/') { return a / b }
+}
