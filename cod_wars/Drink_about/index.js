3ae9370ee7062f73b58494083330ce20522ae1ef
Tima <tima52rus@gmail.com>
2021-11-17 23:22:23 +0200

    ss

---
+++ cod_wars/Drink_about/index.js
@@ -0,0 +1,11 @@
+function peopleWithAgeDrink(old) {
+    if (old <= 13) {
+        return 'drink toddy'
+    } else if (old <= 17) {
+        return 'drink coke'
+    } else if (old <= 20) {
+        return 'drink beer'
+    } else if (old >= 21) {
+        return 'drink whisky'
+    };
+}
