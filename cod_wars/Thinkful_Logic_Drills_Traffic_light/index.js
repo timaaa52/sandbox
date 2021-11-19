23cd1da83d80963f8c2f4e5ba228af0c62d7d81b
Tima <tima52rus@gmail.com>
2021-11-19 20:41:43 +0200

    ss

---
+++ cod_wars/Thinkful_Logic_Drills_Traffic_light/index.js
@@ -0,0 +1,5 @@
+function updateLight(current) {
+
+    //your code here!
+    if (current == 'green') { return 'yellow' } else if (current == 'yellow') { return 'red' } else if (current == 'red') { return 'green' }
+}
